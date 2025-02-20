c34ff126877c91d525f6aae161b43790c15f1a28
Quantin <83812796+lmntrix022@users.noreply.github.com>
2025-02-20 11:15:42 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -14,35 +14,7 @@ const App = () => {
   console.log('Langue préférée de l\'utilisateur :', userLanguage);
 
   
-    const iframe = document.createElement("iframe");
-    
-    const iframeStyles = (styleString) => {
-    const style = document.createElement('style');
-    style.textContent = styleString;
-    document.head.append(style);
-    }
-    
-    iframeStyles('
-        .chat-frame {
-            position: fixed;
-            bottom: 50px;
-            right: 50px;
-            border: none;
-        }
-    ')
-    
-    iframe.src = "http://localhost:3000/chatbot"
-    iframe.classList.add('chat-frame')
-    document.body.appendChild(iframe)
-    
-    window.addEventListener("message", (e) => {
-        if(e.origin !== "http://localhost:3000") return null
-        let dimensions = JSON.parse(e.data)
-        iframe.width = dimensions.width
-        iframe.height = dimensions.height
-        iframe.contentWindow.postMessage("f422b011-ba11-4958-af20-01b7f29d08a0", "http://localhost:3000/")
-    });
-        
+   
  
   return (
     
