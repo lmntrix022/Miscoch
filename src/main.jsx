f30f3ce158363a073fdfbc3a59a34fcefd3e65d0
Quantin <83812796+lmntrix022@users.noreply.github.com>
2025-02-20 11:16:08 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -20,6 +20,36 @@ i18next.init({
   },
 });
 
+ const iframe = document.createElement("iframe");
+    
+    const iframeStyles = (styleString) => {
+    const style = document.createElement('style');
+    style.textContent = styleString;
+    document.head.append(style);
+    }
+    
+    iframeStyles('
+        .chat-frame {
+            position: fixed;
+            bottom: 50px;
+            right: 50px;
+            border: none;
+        }
+    ')
+    
+    iframe.src = "http://localhost:3000/chatbot"
+    iframe.classList.add('chat-frame')
+    document.body.appendChild(iframe)
+    
+    window.addEventListener("message", (e) => {
+        if(e.origin !== "http://localhost:3000") return null
+        let dimensions = JSON.parse(e.data)
+        iframe.width = dimensions.width
+        iframe.height = dimensions.height
+        iframe.contentWindow.postMessage("f422b011-ba11-4958-af20-01b7f29d08a0", "http://localhost:3000/")
+    });
+        
+
 ReactDOM.render(
   <React.StrictMode>
     <I18nextProvider i18n={i18next}>
